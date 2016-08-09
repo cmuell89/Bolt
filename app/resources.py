24d281b791bb3f6e6c7c5ddfe22238406ba3440a
Carl Mueller <cmuell89@gmail.com>
2016-08-09 13:17:01 -0700

    New route expansion with a few changes to other routes.
    New Routes:
    
    Expressions
    post: adds new expressions for the provided intent
    get: gets all eppressions for the provided intent
    
    Updates:
    Post routes will currently only support content-type application JSON

--- app/resources.py
+++ app/resources.py
@@ -5,22 +5,33 @@ Created on Jul 27, 2016
 '''
 from flask import jsonify
 from flask import request
-from database.database import NLP_Database as db
+from database.database import NLP_Database
 from flask_restful import Resource
 from classification.Classification import train_classification_pipeline, classify_document
 
+"""
+Module accessible objects:
+    clf: the NLP classification pipeline built using sk-learn
+    db: the NLP_Database() object used to make calls to the associated Bolt postrgres database
+"""
 clf = train_classification_pipeline()
+db = NLP_Database()
 
 class Classify(Resource):
     def post(self):
         """
         Returns the intent classification of the query.
         """
-        result = str(classify_document(clf, request.get_json()['query']))
-        resp = jsonify(intent=result)
-        resp.status_code = 200
-        return resp
-
+        if request.headers['Content-Type'] == 'application/json':
+            result = str(classify_document(clf, request.get_json()['query']))
+            resp = jsonify(intent=result)
+            resp.status_code = 200
+            return resp
+        else:
+            resp = jsonify(message="Unsupported media type. Currently the API only accepts 'application/json'.")
+            resp.status_code = 415
+            return resp
+        
 class Train(Resource):
     def get(self):
         """
@@ -37,17 +48,42 @@ class Expressions(Resource):
         """
         Adds expression/s to an intent
         """
-        data = request.get_json()
-        result = db.add_expressions_to_intent(self, intent, data['expressions']) 
-        resp = jsonify(result)
-        resp.status_code = 200
-        return resp
-    
+        if request.headers['Content-Type'] == 'application/json':
+            data = request.get_json()
+            result = db.add_expressions_to_intent(intent, data['expressions']) 
+            expressions = [];
+            for res in result:
+                expressions.append(res[0])
+            resp = jsonify(intent=intent,expressions=expressions)
+            resp.status_code = 200
+            return resp
+        else:
+            resp = jsonify(message="Unsupported media type. Currently the API only accepts 'application/json'.")
+            resp.status_code = 415
+            return resp
+            
     def get(self, intent):
         """
         Returns the expressions for an intent
         """ 
-        result = db.add_expressions_to_intent(self, intent) 
-        resp = jsonify(result)
+        result = db.get_intent_expressions(intent)
+        expressions = []
+        for res in result:
+            expressions.append(res[0])
+        resp = jsonify(intent=intent,expressions=expressions)
         resp.status_code = 200
         return resp
+
+class Intents(Resource):
+    def post(self):
+        """
+        Adds an intent to postgres
+        """
+        data = request.get_json()
+        result = db.add_intent(data['intent'])
+        resp = jsonify(result)
+        
+        
+        
+        
+        
