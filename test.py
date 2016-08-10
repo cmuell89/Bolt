8da50fd17467770e43537dd89d18dc0d84bccb1b
Carl Mueller <cmuell89@gmail.com>
2016-08-10 16:13:01 -0700

    logging module

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ Created on Jul 27, 2016
 @author: carl
 '''
 import unittest
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
     testsuite = unittest.TestLoader().discover('./test')
