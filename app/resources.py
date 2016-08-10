59b357ddd07fcfc60fbdc700d7c8ec36cdc34527
Carl Mueller <cmuell89@gmail.com>
2016-08-10 16:11:43 -0700

    Execption handling for database methods in routes. New get route under Intents resrouce class.

--- app/resources.py
+++ app/resources.py
@@ -8,6 +8,8 @@ from flask import request
 from database.database import NLP_Database
 from flask_restful import Resource
 from classification.Classification import train_classification_pipeline, classify_document
+from exceptions import DatabaseError, DatabaseInputError
+
 
 """
 Module accessible objects:
@@ -47,16 +49,24 @@ class Expressions(Resource):
     def post(self, intent):
         """
         Adds expression/s to an intent
+        Currently only supports 'application/json' mimetype.
         """
         if request.headers['Content-Type'] == 'application/json':
             data = request.get_json()
-            result = db.add_expressions_to_intent(intent, data['expressions']) 
-            expressions = [];
-            for res in result:
-                expressions.append(res[0])
-            resp = jsonify(intent=intent,expressions=expressions)
-            resp.status_code = 200
-            return resp
+            try:
+                result = db.add_expressions_to_intent(intent, data['expressions'])
+                expressions = list(map(lambda tup: tup[0], result)) 
+                resp = jsonify(intent=intent,expressions=expressions)
+                resp.status_code = 200
+                return resp
+            except DatabaseError as error:
+                resp = jsonify(error=error.value)
+                resp.status_code = 500
+                return resp
+            except DatabaseInputError as error:
+                resp = jsonify(error=error.value)
+                resp.status_code = 400
+                return resp
         else:
             resp = jsonify(message="Unsupported media type. Currently the API only accepts 'application/json'.")
             resp.status_code = 415
@@ -66,24 +76,78 @@ class Expressions(Resource):
         """
         Returns the expressions for an intent
         """ 
-        result = db.get_intent_expressions(intent)
-        expressions = []
-        for res in result:
-            expressions.append(res[0])
-        resp = jsonify(intent=intent,expressions=expressions)
-        resp.status_code = 200
-        return resp
-
+        try:
+            result = db.get_intent_expressions(intent)
+            expressions = list(map(lambda tup: tup[0], result)) 
+            resp = jsonify(intent=intent,expressions=expressions)
+            resp.status_code = 200
+            return resp
+        except DatabaseError as error:
+            resp = jsonify(error=error.value)
+            resp.status_code = 500
+            return resp
+        except DatabaseInputError as error:
+            resp = jsonify(error=error.value)
+            resp.status_code = 400
+            return resp
+        
+    
+#     def delete(self, intent):
+#         """
+#         Deletes an expression/s from an intent
+#         """
+#         if request.headers['Content-Type'] == 'application/json':
+#             data = request.get_json()
+#             if data['all'] == True:
+#                 try:
+#                     result = db.delete_all_intent_expressions(intent)
+#                     
+#                     resp = jsonify()
+#                 except DatabaseError as error:
+#                     resp = jsonify(error=error.value)
+#                     resp.status_code = 500
+#                     return resp
+#                 except DatabaseInputError as error:
+#                     resp = jsonify(error=error.value)
+#                     resp.status_code = 400
+#                     return resp   
+#             else:
+#                 result = db.delete_expression_from_intent(intent, data['expressions'])
+           
+            
 class Intents(Resource):
     def post(self):
         """
-        Adds an intent to postgres
+        Posts an intent to NLP postgres database.
+        Currently only supports 'application/json' mimetype.
         """
-        data = request.get_json()
-        result = db.add_intent(data['intent'])
-        resp = jsonify(result)
+        if request.headers['Content-Type'] == 'application/json':
+            data = request.get_json()
+            try:
+                result = db.add_intent(data['intent'])
+                intents = list(map(lambda tup: tup[0], result))
+                resp = jsonify(intents=intents)
+                resp.status_code = 200
+                return resp
+            except DatabaseError as error:
+                resp = jsonify(error=error)
+                resp.status_code = 500
+                return resp
+            except DatabaseInputError as error:
+                resp = jsonify(error=error)
+                resp.status_code = 400
+                return resp
+        else:
+            resp = jsonify(message="Unsupported media type. Currently the API only accepts 'application/json'.")
+            resp.status_code = 415
+            return resp
         
-        
-        
-        
-        
+    def get(self):
+        """
+        Gets the current intents stored in the NLP database
+        """
+        result = db.get_intents()
+        intents = list(map(lambda tup: tup[0], result))
+        resp = jsonify(intents=intents)
+        resp.status_code = 200
+        return resp
