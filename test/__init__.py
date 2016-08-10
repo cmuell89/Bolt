1b778c0720354523865b97c694e576b1966b1154
Carl Mueller <cmuell89@gmail.com>
2016-08-10 16:13:39 -0700

    Added Intents_Test testsuite to test_cases

--- test/__init__.py
+++ test/__init__.py
@@ -1,9 +1,9 @@
 from unittest import TestSuite
-from test.test_app import Classification_Test, Expressions_Test
+from test.test_app import Classification_Test, Expressions_Test, Intents_Test
 from test.test_general import Classifier_Test, NLP_Database_Test
 
 
-test_cases = (Classification_Test, Expressions_Test, Classifier_Test, NLP_Database_Test)
+test_cases = (Classification_Test, NLP_Database_Test, Expressions_Test, Intents_Test, Classifier_Test)
 
 def load_tests(loader, tests, pattern):
     suite = TestSuite()
