4d2fdfbf921a8036638d64ded9695c33d379b322
Carl Mueller <cmuell89@gmail.com>
2016-08-11 17:18:45 -0700

    setUpClass methods and tests for delete epxressions and intents routes

--- test/test_app.py
+++ test/test_app.py
@@ -5,7 +5,9 @@ Created on Jul 27, 2016
 '''
 import unittest
 import json
+from database.database import NLP_Database
 from app import app
+
 
     
 class Classification_Test(unittest.TestCase):
@@ -33,7 +35,6 @@ class Classification_Test(unittest.TestCase):
         print("\n\tTesting 'POST' '/classification/classify' route a success!")
     
     def test_train_route(self):
-        
         response = self.app.get('/classification/train')  
         self.assertEqual(response.status_code, 200) 
         result = json.loads(response.get_data(as_text=True))
@@ -49,28 +50,49 @@ class Expressions_Test(unittest.TestCase):
     def setUpClass(self):
         self.app = app.test_client()
         self.app.testing = True
-        print("\nRoute testing for 'expressions/<string:intent>' endpoints:")
+        self.db = NLP_Database()
+        self.db.add_intent('test-intent')
+        self.db.add_expressions_to_intent('test-intent', ["test expression one", "test expression two", "test expression three", "test expression four"])
+        self.db.add_intent('delete-intent')
+        self.db.add_expressions_to_intent('delete-intent', ["one","two","three","four"])
+        print("\nRoute testing for 'database/expressions/<string:intent>' endpoints:")
         
     @classmethod
     def tearDownClass(self):
-        pass
+        self.db.delete_intent('delete-intent')
+        self.db.delete_intent('test-intent')
+        self.db.close_database_connection()
     
     def test_post_expressions_to_intent_route(self):
-        expressions = ["get order #12341234"]
-        response = self.app.post('/database/expressions/get-order', data=json.dumps(dict(expressions=expressions)), content_type='application/json')
+        expressions = ["test expression five", "test expression six", "test expression seven"]
+        response = self.app.post('/database/expressions/test-intent', data=json.dumps(dict(expressions=expressions)), content_type='application/json')
         result = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
-        self.assertIn("get order #12341234", result['expressions']) 
-        secondResponse = self.app.post('/database/expressions/get-order', data=json.dumps(dict(expressions=expressions)))
+        self.assertIn("test expression five", result['expressions']) 
+        secondResponse = self.app.post('/database/expressions/test-intent', data=json.dumps(dict(expressions=expressions)))
         self.assertEqual(secondResponse.status_code, 415) 
         print("\n\tTesting 'POST' '/database/expressions/<intent>' route a success!")
         
     def test_get_intent_expressions_route(self):
-        response = self.app.get('/database/expressions/get-order')  
-        result = json.loads(response.get_data(as_text=True)) 
+        response = self.app.get('/database/expressions/test-intent')  
+        result = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
-        self.assertIn("what is order 3216", result['expressions']) 
+        self.assertIn("test expression two", result['expressions']) 
         print("\n\tTesting 'GET' '/database/expressions/<intent>' route a success!")
+        
+    def test_delete_intent_expressions_route(self):
+        response = self.app.delete('/database/expressions/delete-intent', data=json.dumps(dict(all=False, expressions=["one"])), content_type='application/json')
+        result = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn("one", result['expressions'])
+        secondResponse = self.app.delete('/database/expressions/delete-intent', data=json.dumps(dict(all=True)), content_type='application/json')
+        secondResult = json.loads(secondResponse.get_data(as_text=True))
+        self.assertEqual(secondResponse.status_code, 200)
+        self.assertTrue(len(secondResult['expressions'])==0, secondResult['expressions'])
+        thirdResponse = self.app.delete('/database/expressions/delete-intent', data=json.dumps(dict(all=True)))
+        self.assertEqual(thirdResponse.status_code, 415) 
+        print("\n\tTesting 'DELETE' '/database/expressions/*' route a success!")
+               
                  
 class Intents_Test(unittest.TestCase):
     """
@@ -80,11 +102,13 @@ class Intents_Test(unittest.TestCase):
     def setUpClass(self):
         self.app = app.test_client()
         self.app.testing = True
+        self.db = NLP_Database()
+        self.db.add_intent('to-be-deleted')
         print("\nRoute testing for '/database/intents/' endpoints:")
         
     @classmethod
     def tearDownClass(self):
-        pass
+        self.db.close_database_connection()
     
     def test_post_intent_route(self):
         intent = "some-new-intent"
@@ -92,14 +116,21 @@ class Intents_Test(unittest.TestCase):
         result = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
         self.assertIn("some-new-intent", result['intents'])
+        self.db.delete_intent(intent)
         secondResponse = self.app.post('/database/intents', data=json.dumps(dict(intent=intent)))
         self.assertEqual(secondResponse.status_code, 415)
-        print("\nTesting 'POST' '/database/intents/ route a success!")
+        print("\n\tTesting 'POST' '/database/intents/ route a success!")
         
-    def test_get_intents(self):
+    def test_get_intents_route(self):
         response = self.app.get('/database/intents')
         result = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
         self.assertIn("get-order", result['intents'])
-        print("\nTesting 'GET' '/database/intents/ route a success!")
+        print("\n\tTesting 'GET' '/database/intents/ route a success!")
+        
+    def test_delete_intent_route(self):
+        response = self.app.delete('/database/intents', data=json.dumps(dict(intent='to-be-deleted')), content_type='application/json')
+        result = json.loads(response.get_data(as_text=True))
+        self.assertNotIn('to-be-deleted', result['intents'])
+        print("\n\tTesting 'DELETE' '/database/intents/ route a success!")
     
