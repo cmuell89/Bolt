c53fb47aa9e4a878d9a9aa9955cbf2dfad864354
Carl Mueller <cmuell89@gmail.com>
2016-09-12 12:27:59 -0700

    Now using dotenv again (checks if file exists)

--- application.py
+++ application.py
@@ -6,26 +6,26 @@ Created on Jul 18, 2016
 import os
 import logging
 from app import app as application
-# from os.path import join, dirname
-# from dotenv import load_dotenv
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 '''
 Loads environment variables.
 '''
-# dotenv_path = join(dirname(__file__), '.env')
-# load_dotenv(dotenv_path)
-# logger = logging.getLogger('bolt')
-# 
-# if os.environ.get('ENVIRONMENT')=='dev':
-#     logger.setLevel(logging.DEBUG)
-#     logger.info("Running in dev mode using eclipse/python interpreter")
-#     application.run(host=os.environ.get('HOST'), debug=True, use_reloader=True)
-# elif os.environ.get('ENVIRONMENT')=='test':
-#     logger.setLevel(logging.INFO)
-#     logger.info("Running in test mode using eclipse/python interpreter")
-#     application.run(host=os.environ.get('HOST'), port=os.environ.get('PORT'), debug=False, use_reloader=False)
-# elif os.environ.get('ENVIRONMENT'=='prod'):
-#     logger.setLevel(logging.INFO)
-#     logger.info("Running in production mode...")
-#     application.run()
-application.run()
+dotenv_path = join(dirname(__file__), '.env')
+if os.path.isfile(dotenv_path) == True:
+    load_dotenv(dotenv_path)
+logger = logging.getLogger('bolt')
+ 
+if os.environ.get('ENVIRONMENT')=='dev':
+    logger.setLevel(logging.DEBUG)
+    logger.info("Running in dev mode using eclipse/python interpreter")
+    application.run(host=os.environ.get('HOST'), debug=True, use_reloader=True)
+elif os.environ.get('ENVIRONMENT')=='test':
+    logger.setLevel(logging.INFO)
+    logger.info("Running in test mode using eclipse/python interpreter")
+    application.run(host=os.environ.get('HOST'), port=os.environ.get('PORT'), debug=False, use_reloader=False)
+elif os.environ.get('ENVIRONMENT'=='prod'):
+    logger.setLevel(logging.INFO)
+    logger.info("Running in production mode...")
+    application.run()
