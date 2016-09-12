cad008b7963c7b8e08a7fca235132a04e821d02c
Carl Mueller <cmuell89@gmail.com>
2016-09-12 16:45:28 -0700

    if name == main statement in application.py

--- application.py
+++ application.py
@@ -9,16 +9,18 @@ import logging
 from app import app as application
 
 logger = logging.getLogger('bolt')
- 
-if os.environ.get('ENVIRONMENT')=='dev':
-    logger.setLevel(logging.DEBUG)
-    logger.info("Running in dev mode using eclipse/python interpreter")
-    application.run(host=os.environ.get('DEV_HOST'),port=int(os.environ.get('DEV_PORT')), debug=True, use_reloader=True)
-elif os.environ.get('ENVIRONMENT')=='test':
-    logger.setLevel(logging.INFO)
-    logger.info("Running in test mode using eclipse/python interpreter")
-    application.run(debug=True, use_reloader=False)
-elif os.environ.get('ENVIRONMENT')=='prod':
-    logger.setLevel(logging.INFO)
-    logger.info("Running in production mode...")
-    application.run()
+
+# run the app.
+if __name__ == "__main__": 
+    if os.environ.get('ENVIRONMENT')=='dev':
+        logger.setLevel(logging.DEBUG)
+        logger.info("Running in dev mode using eclipse/python interpreter")
+        application.run(host=os.environ.get('DEV_HOST'),port=int(os.environ.get('DEV_PORT')), debug=True, use_reloader=True)
+    elif os.environ.get('ENVIRONMENT')=='test':
+        logger.setLevel(logging.INFO)
+        logger.info("Running in test mode using eclipse/python interpreter")
+        application.run(debug=True, use_reloader=False)
+    elif os.environ.get('ENVIRONMENT')=='prod':
+        logger.setLevel(logging.INFO)
+        logger.info("Running in production mode...")
+        application.run()
