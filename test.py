9009fd38ba90689f18aad3d8fae1f911310c2029
Carl Mueller <cmuell89@gmail.com>
2016-09-21 10:37:02 -0700

    Removing yum packages to see if AMI image allows easy deploy

--- test.py
+++ test.py
@@ -5,9 +5,13 @@ Created on Jul 27, 2016
 '''
 import unittest
 import logging
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.DEBUG)
-
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+            
 if __name__ == '__main__':
     testsuite = unittest.TestLoader().discover('./test')
     unittest.TextTestRunner(verbosity=1).run(testsuite)
