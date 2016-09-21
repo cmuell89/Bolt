b48e10bf33d42e002eb8f42ceaeb98ee88131d89
Carl Mueller <cmuell89@gmail.com>
2016-09-21 16:38:47 -0700

    update schemas

--- database/schemas.py
+++ database/schemas.py
@@ -4,19 +4,19 @@ Created on Sep 19, 2016
 @author: carl
 '''
 
-from sqlalchemy.ext.delcarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Text, ForeignKey
 
 
 Base = declarative_base()
 
 class Expressions(Base):
     __tablename__ = 'expressions'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True)
     expressions = Column(Text)
-    intent_id = Column(Integer)
+    intent_id = Column(Integer, ForeignKey("intents.id"))
     
 class Intents(Base):
     __tablename__ = 'intents'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True)
     intents = Column(Text)
