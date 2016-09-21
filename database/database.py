0af9c74d3cfbbb162b78cd2ea2274dbcd63c4911
Carl Mueller <cmuell89@gmail.com>
2016-09-21 16:56:55 -0700

    Changed all database queries to use public schema rather than nlp schema of the test db. test db will have to be remade.

--- database/database.py
+++ database/database.py
@@ -27,7 +27,7 @@ class NLP_Database:
 
     def get_intents(self):
         try:
-            self.cur.execute("SELECT nlp.intents.intents FROM nlp.intents;")
+            self.cur.execute("SELECT public.intents.intents FROM public.intents;")
             logger.info("Retrieving all intents")
             return self.cur.fetchall()
         except psycopg2.Error as e:
@@ -38,7 +38,7 @@ class NLP_Database:
     def get_intent_expressions(self,intent):
         if intent:
             try:
-                self.cur.execute("SELECT nlp.expressions.expressions FROM nlp.intents INNER JOIN nlp.expressions ON nlp.intents.id = nlp.expressions.intent_id WHERE nlp.intents.intents = %s;", (intent,))
+                self.cur.execute("SELECT public.expressions.expressions FROM public.intents INNER JOIN public.expressions ON public.intents.id = public.expressions.intent_id WHERE public.intents.intents = %s;", (intent,))
                 logger.info("Retrieving all expressions for the intent: %s", intent)
                 return self.cur.fetchall()
             except psycopg2.Error as e:
@@ -53,7 +53,7 @@ class NLP_Database:
     
     def get_intents_and_expressions(self):
         try:
-            self.cur.execute("SELECT nlp.intents.intents, nlp.expressions.expressions FROM nlp.intents INNER JOIN nlp.expressions ON nlp.intents.id = nlp.expressions.intent_id;")
+            self.cur.execute("SELECT public.intents.intents, public.expressions.expressions FROM public.intents INNER JOIN public.expressions ON public.intents.id = public.expressions.intent_id;")
             logger.info("Retrieving all intents and expressions.")
             return self.cur.fetchall()
         except psycopg2.Error as e:
@@ -63,7 +63,7 @@ class NLP_Database:
     
     def add_intent(self, intent):
         try:
-            self.cur.execute("INSERT INTO nlp.intents (intents) VALUES (%s);", (intent,))
+            self.cur.execute("INSERT INTO public.intents (intents) VALUES (%s);", (intent,))
             self.conn.commit()
             logger.info("Adding intent: %s", intent)
             return self.get_intents()
@@ -75,11 +75,11 @@ class NLP_Database:
     def add_expressions_to_intent(self, intent, expressions):
         if intent:
             try:
-                self.cur.execute("SELECT nlp.intents.id FROM nlp.intents WHERE nlp.intents.intents = %s;", (intent,))
+                self.cur.execute("SELECT public.intents.id FROM public.intents WHERE public.intents.intents = %s;", (intent,))
                 intentID = self.cur.fetchone()
                 if len(expressions) > 0:
                     for expression in expressions:
-                        self.cur.execute("INSERT INTO nlp.expressions (expressions, intent_id) VALUES (%s, %s)", (expression, intentID))
+                        self.cur.execute("INSERT INTO public.expressions (expressions, intent_id) VALUES (%s, %s)", (expression, intentID))
                         self.conn.commit()
                     logger.info("Adding expressions to intent: %s", intent)
                     return self.get_intent_expressions(intent)
@@ -99,7 +99,7 @@ class NLP_Database:
     def delete_intent(self, intent):
         try:
             self.delete_all_intent_expressions(intent)
-            self.cur.execute("DELETE FROM nlp.intents WHERE nlp.intents.intents = %s;", (intent,))
+            self.cur.execute("DELETE FROM public.intents WHERE public.intents.intents = %s;", (intent,))
             self.conn.commit()
             logger.info("Deleting intent: %s", intent)
             return self.get_intents()
@@ -110,7 +110,7 @@ class NLP_Database:
     
     def delete_all_intent_expressions(self, intent):
         try:
-            self.cur.execute("DELETE FROM nlp.expressions WHERE nlp.expressions.intent_id = (SELECT id FROM nlp.intents WHERE nlp.intents.intents = %s);", (intent,))
+            self.cur.execute("DELETE FROM public.expressions WHERE public.expressions.intent_id = (SELECT id FROM public.intents WHERE public.intents.intents = %s);", (intent,))
             self.conn.commit()
             logger.info("Deleting all expressions from intent: %s", intent)
             return self.get_intent_expressions(intent)
@@ -122,7 +122,7 @@ class NLP_Database:
     def delete_expressions_from_intent(self, intent, expressions):
         try:
             for expression in expressions:
-                self.cur.execute("DELETE FROM nlp.expressions WHERE nlp.expressions.intent_id = (SELECT id FROM nlp.intents WHERE nlp.intents.intents = %s) AND nlp.expressions.expressions = %s;", (intent, expression))
+                self.cur.execute("DELETE FROM public.expressions WHERE public.expressions.intent_id = (SELECT id FROM public.intents WHERE public.intents.intents = %s) AND public.expressions.expressions = %s;", (intent, expression))
                 logger.info("Deleting the expression: '%s' from intent: %s", expression, intent)
                 self.conn.commit()
             return self.get_intent_expressions(intent)
