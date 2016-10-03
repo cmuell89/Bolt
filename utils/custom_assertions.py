664b3ba35b9b95c1f2542d7e002af7a69082e8e2
Carl Mueller <cmuell89@gmail.com>
2016-10-03 15:35:13 -0700

    updated assertListOfTuples to be able to receive an array of the tuple types necessary

--- utils/custom_assertions.py
+++ utils/custom_assertions.py
@@ -9,15 +9,22 @@ class CustomAssertions():
     Mixin class for creating custom assertions used in this testing module.
     """
     
-    def assertListOfTuples(self, obj):
-        if isinstance(obj, str):
-            raise AssertionError("Returned a string argument but expected a list of tuples of length 2")
+    def assertListOfTuples(self, obj, typeArray):
+        """
+        typeArray is an array of types used for the comparison of the obj tuple.
+            len(obj) == len(typeArray)
+            type(obj[0]) == typeArray[0]
+        """
+        if not isinstance(obj, list):
+            raise AssertionError("Object is not a list.")
+        if not isinstance(obj[0], tuple):
+            raise AssertionError("Object does not contain tuples")
         if len(obj) == 0:
             raise AssertionError("Method failed to return a populated list.")
         else:
-            assert len(obj[0]) == 2
-            assert isinstance(obj[0][0], str)
-            assert isinstance(obj[0][1], str)
+            assert len(obj[0]) == len(typeArray)
+            for idx in range(len(obj[0])):
+                assert isinstance(obj[0][idx], typeArray[idx])
             
     def assertListOfString(self, obj):
         if isinstance(obj, str):
