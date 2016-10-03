545c1fee1ebc63b22ad85a070ccd2d9121b5550b
Carl Mueller <cmuell89@gmail.com>
2016-10-03 15:36:53 -0700

    database results mapped via lambda expressions into python lists. Added new database methods to get, add, and delete unlableled expressions into new unlabeled expressions database table

--- database/database.py
+++ database/database.py
@@ -31,35 +31,54 @@ class NLP_Database:
 
     def get_intents(self):
         try:
-            self.cur.execute("SELECT public.intents.intents FROM public.intents;")
-            logger.debug("Retrieving all intents")
-            return self.cur.fetchall()
+            self.cur.execute("SELECT intents FROM public.intents;")
+            logger.debug("Retrieving all intents") 
+            listOfIntents = list(map(lambda x: x[0], self.cur.fetchall()))          
+            return listOfIntents 
         except psycopg2.Error as e:
             self.conn.rollback()
             logger.exception(e.pgerror)
             raise DatabaseError(e.pgerror)
 
     def get_intent_expressions(self,intent):
-        if intent:
+        try: 
+            self.cur.execute("SELECT id FROM public.intents WHERE public.intents.intents = %s;", (intent,))
+            intentID = self.cur.fetchone()
+        except psycopg2.Error as e:
+                raise DatabaseError(e.pgerror)
+        if intentID is not None:
             try:
-                self.cur.execute("SELECT public.expressions.expressions FROM public.intents INNER JOIN public.expressions ON public.intents.id = public.expressions.intent_id WHERE public.intents.intents = %s;", (intent,))
+                self.cur.execute("SELECT expressions FROM public.intents INNER JOIN public.expressions ON public.intents.id = public.expressions.intent_id WHERE public.intents.intents = %s;", (intent,))
                 logger.debug("Retrieving all expressions for the intent: %s", intent)
-                return self.cur.fetchall()
+                listOfExpressions = list(map(lambda x: x[0], self.cur.fetchall()))
+                return listOfExpressions
             except psycopg2.Error as e:
                 self.conn.rollback()
                 logger.exception(e.pgerror)
                 raise DatabaseError(e.pgerror)
         else:
-            msg = "Method expects valid intent string as argument"
+            msg = "Method expects valid/existing intent as argument"
             logger.exception(msg)
             raise DatabaseInputError(msg)
                 
     
     def get_intents_and_expressions(self):
         try:
-            self.cur.execute("SELECT public.intents.intents, public.expressions.expressions FROM public.intents INNER JOIN public.expressions ON public.intents.id = public.expressions.intent_id;")
+            self.cur.execute("SELECT intents, expressions FROM public.intents INNER JOIN public.expressions ON public.intents.id = public.expressions.intent_id;")
             logger.debug("Retrieving all intents and expressions.")
-            return self.cur.fetchall()
+            intentsAndExpressions = list(map(lambda x: (x[0], x[1]), self.cur.fetchall()))
+            return intentsAndExpressions
+        except psycopg2.Error as e:
+            self.conn.rollback()
+            logger.exception(e.pgerror)
+            raise DatabaseError(e.pgerror)
+        
+    def get_unlabeled_expressions(self):
+        try:
+            self.cur.execute("SELECT id, expressions, estimated_intent FROM public.unlabeled_expressions")
+            logger.debug("Retrieving all unlabeled expressions.")
+            unlabeledExpressions = list(map(lambda x: (x[0], x[1], x[2]), self.cur.fetchall()))
+            return unlabeledExpressions
         except psycopg2.Error as e:
             self.conn.rollback()
             logger.exception(e.pgerror)
@@ -77,15 +96,18 @@ class NLP_Database:
             raise DatabaseError(e.pgerror)
     
     def add_expressions_to_intent(self, intent, expressions):
-        if intent:
+        try: 
+            self.cur.execute("SELECT id FROM public.intents WHERE public.intents.intents = %s;", (intent,))
+            intentID = self.cur.fetchone()
+        except psycopg2.Error as e:
+                raise DatabaseError(e.pgerror)
+        if intentID is not None:
             try:
-                self.cur.execute("SELECT public.intents.id FROM public.intents WHERE public.intents.intents = %s;", (intent,))
-                intentID = self.cur.fetchone()
                 if len(expressions) > 0:
+                    logger.debug("Adding expressions to intent: %s", intent)
                     for expression in expressions:
                         self.cur.execute("INSERT INTO public.expressions (expressions, intent_id) VALUES (%s, %s)", (expression, intentID))
-                        self.conn.commit()
-                    logger.debug("Adding expressions to intent: %s", intent)
+                    self.conn.commit()
                     return self.get_intent_expressions(intent)
                 else:
                     msg = "Method expects a non-empty list of expressions"
@@ -96,10 +118,31 @@ class NLP_Database:
                 logger.exception(e.pgerror)
                 raise DatabaseError(e.pgerror)
         else:
-            msg = "Method expects valid intent string as argument"
+            msg = "Method expects valid/existing intent as argument"
             logger.exception(msg)
             raise DatabaseInputError(msg)
-            
+    
+    def add_unlabeled_expression(self, expression, estimatedIntent):
+        if expression:
+            try:
+                if len(expression) > 0:
+                    logger.debug("Adding unlabeled expression to database.")
+                    self.cur.execute("INSERT INTO public.unlabeled_expressions (expressions, estimated_intent) VALUES (%s, %s)", (expression, estimatedIntent))
+                    self.conn.commit()
+                    return self.get_unlabeled_expressions()
+                else:
+                    msg = "Method expects an unlabeled expression of length > 0."
+                    logger.exception(msg)
+                    raise DatabaseInputError(msg)
+            except psycopg2.Error as e:
+                self.conn.rollback()
+                logger.exception(p.pgerror)
+                raise DatabaseError
+        else:
+            msg = "Method expects valid expression string as argument"
+            logger.exception(msg)
+            raise DatabaseInputError(msg)
+                 
     def delete_intent(self, intent):
         try:
             self.delete_all_intent_expressions(intent)
@@ -114,7 +157,7 @@ class NLP_Database:
     
     def delete_all_intent_expressions(self, intent):
         try:
-            self.cur.execute("DELETE FROM public.expressions WHERE public.expressions.intent_id = (SELECT id FROM public.intents WHERE public.intents.intents = %s);", (intent,))
+            self.cur.execute("DELETE FROM expressions WHERE public.expressions.intent_id = (SELECT id FROM public.intents WHERE public.intents.intents = %s);", (intent,))
             self.conn.commit()
             logger.debug("Deleting all expressions from intent: %s", intent)
             return self.get_intent_expressions(intent)
@@ -126,7 +169,7 @@ class NLP_Database:
     def delete_expressions_from_intent(self, intent, expressions):
         try:
             for expression in expressions:
-                self.cur.execute("DELETE FROM public.expressions WHERE public.expressions.intent_id = (SELECT id FROM public.intents WHERE public.intents.intents = %s) AND public.expressions.expressions = %s;", (intent, expression))
+                self.cur.execute("DELETE FROM expressions WHERE public.expressions.intent_id = (SELECT id FROM public.intents WHERE public.intents.intents = %s) AND public.expressions.expressions = %s;", (intent, expression))
                 logger.debug("Deleting the expression: '%s' from intent: %s", expression, intent)
                 self.conn.commit()
             return self.get_intent_expressions(intent)
@@ -134,7 +177,16 @@ class NLP_Database:
             self.conn.rollback()
             logger.exception(e.pgerror)
             raise DatabaseError(e.pgerror)
-
+    
+    def delete_unlabeled_expression(self, id):
+        try:
+            self.cur.execute("DELETE FROM unlabeled_expressions WHERE public.unlabeled_expressions.id = %s", (id,))
+            self.conn.commit()
+            return self.get_unlabeled_expressions()
+        except psycopg2.Error as e:
+            self.conn.rollback()
+            logger.exception(e.pgerror)
+            raise DatabaseError(e.pgerror)
         
     def close_database_connection(self):
         self.cur.close()
