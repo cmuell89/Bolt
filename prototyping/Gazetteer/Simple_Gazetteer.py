368f844fb8f88d8f4be71f6364d36ed190009f4e
Carl Mueller <cmuell89@gmail.com>
2016-11-29 16:18:11 -0800

    simple gazetter and further spacy exloration testing

--- prototyping/Gazetteer/Simple_Gazetteer.py
+++ prototyping/Gazetteer/Simple_Gazetteer.py
@@ -3,21 +3,25 @@ Created on Oct 14, 2016
 
 @author: carl
 '''
-import re
+import settings
+import json
+from nlp.ner.gazetteer import SimpleTrieBuilder, SimpleGazetteer, Gazetteer, GramTrieBuilder
+
 
 product_types = []
-with open('../../resources/product_types.json') as f:
-    for line in f:
-        product_types.append(line)
+with open('../../resources/product_lists/product_types.json') as f:
+    file_data = json.load(f)
+    for product_type in file_data['product_types']:
+        print(product_type)
+        product_types.append(product_type)
 
+trie_builder = SimpleTrieBuilder()
+trie = trie_builder.build_simple_trie_from_dictionary(product_types)
 
+gazetteer = Gazetteer(trie)
 
-""" Create vocab and product name lists"""
-for line in f:
-    new_line = line.replace("\"", "").replace(",", "").replace("\n", "").replace("\'", "").replace(" - "," ")
-    new_line = re.sub(r'\s+', ' ', new_line).strip()
-    product_names.append(new_line)
-    new_line = new_line.replace("&","")
-    cleaned_product_names.append(new_line)
-    words = new_line.split(' ')
-    vocab.extend(words)
+query = ""
+while query != 'exit':
+    query = input("Test query: ")
+    results = gazetteer.search_query(query, ['what', 'are', 'best', 'selling'])
+    print(results)
--- prototyping/SpaCyExploration.py
+++ prototyping/SpaCyExploration.py
@@ -14,24 +14,24 @@ from spacy.en import English
 #########################
 # Function that returns an array of arrays of training data. First array element is the array of training documents, the second is the labels.
 def create_training_data_from_JSON(fileAddress):
-	file = open(fileAddress)
-	data = json.load(file)
-	intents = data["intents"]
-	docs = []
-	labels = []
-	for intent in intents:
-		docs = docs + intent["expressions"]
-		for i in range(len(intent["expressions"])):
-			labels.append(intent["name"])
-	return [docs,labels]
+    file = open(fileAddress)
+    data = json.load(file)
+    intents = data["intents"]
+    docs = []
+    labels = []
+    for intent in intents:
+        docs = docs + intent["expressions"]
+        for i in range(len(intent["expressions"])):
+            labels.append(intent["name"])
+    return [docs, labels]
 
 def dependency_labels_to_root(token):
 # ''Walk up the syntactic tree, collecting the arc labels.'''
-	dep_labels = []
-	while token.head is not token:
-		dep_labels.append(token.dep)
-		token = token.head
-	return dep_labels
+    dep_labels = []
+    while token.head is not token:
+        dep_labels.append(token.dep)
+        token = token.head
+    return dep_labels
 
 ##########
 # SCRIPT #
@@ -48,17 +48,17 @@ train = trainingData[0]
 parsedDocs =  [parser(x) for x in train]
 labelsTrain = trainingData[1]
 
-	
+    
 # sents = [];
 # for doc in parsedDocs:
-# 	print(doc)
-# 	for chunk in doc.noun_chunks:
-# 		print(chunk.label_, chunk.orth_, '<--', chunk.root.head.orth_)
-# 	for token in doc:
-# 		print(token.orth_, '-->',token.pos_)
-# 	print(doc.ents)
-# 	print()
-# 	print()
+#     print(doc)
+#     for chunk in doc.noun_chunks:
+#         print(chunk.label_, chunk.orth_, '<--', chunk.root.head.orth_)
+#     for token in doc:
+#         print(token.orth_, '-->',token.pos_)
+#     print(doc.ents)
+#     print()
+#     print()
 
 print(parsedDocs[0])
 print(parsedDocs[450])
@@ -88,19 +88,19 @@ print(parsedDocs[0].similarity(parsedDocs[450]))
 # test
 query = 'some string'
 while(query != '0'):
-	query = input('Enter query: ')
-	doc = parser(query)
-	for chunk in doc.noun_chunks:
-		print()
-		print(chunk.label_, chunk.orth_, '<--', chunk.root.head.orth_)
-	
-	print()
-	
-	for token in doc:
-		print(token.orth_, " --> ", token.tag_)
-		print(token.ent_type_)
-		
-	print()
+    query = input('Enter query: ')
+    doc = parser(query)
+    for chunk in doc.noun_chunks:
+        print()
+        print(chunk.label_, chunk.orth_, '<--', chunk.root.head.orth_)
+    
+    print()
+    
+    for token in doc:
+        print(token.orth_, " --> ", token.tag_)
+        print(token.ent_type_)
+        
+    print()
 
 
 
