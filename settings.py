715ca9287d7c3f5bd11e2aef493806e537e7c8e0
Carl Mueller <cmuell89@gmail.com>
2016-12-02 13:17:27 -0800

    added stdout handler to werkzeug

--- settings.py
+++ settings.py
@@ -15,32 +15,35 @@ if os.path.isfile(os.path.join(os.path.dirname(__file__), '.env')):
 """
 LOGGING
 """
-logger = logging.getLogger('BOLT')
+application_logger = logging.getLogger('BOLT')
+werkzeug_logger = logging.getLogger('werkzeug')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 stdoutHandler = logging.StreamHandler(sys.stdout)
 stdoutHandler.setFormatter(formatter)
-logger.addHandler(stdoutHandler)
+application_logger.addHandler(stdoutHandler)
+werkzeug_logger.addHandler(stdoutHandler)
 
 if os.environ.get('ENVIRONMENT') == 'dev':
     fileHandler = logging.FileHandler("{0}/{1}.log".format(os.path.join(os.path.dirname(__file__), './logs'), 'stdout'))
     fileHandler.setFormatter(formatter)
-    logger.addHandler(fileHandler)
+    application_logger.addHandler(fileHandler)
 
 paperTrailsHandler = logging.handlers.SysLogHandler(address=(os.environ.get('PAPERTRAILS_ADDRESS')
                                                              , int(os.environ.get('PAPERTRAILS_PORT'))))
 paperTrailsHandler.setFormatter(formatter)
-logger.addHandler(paperTrailsHandler)
-
-werkzeug_logger = logging.getLogger('werkzeug')
+application_logger.addHandler(paperTrailsHandler)
 werkzeug_logger.addHandler(paperTrailsHandler)
 
 if os.environ.get('ENVIRONMENT') == 'dev':
-    logger.setLevel(logging.DEBUG)
-    logger.info("Running in development mode. Logging with level DEBUG")
+    application_logger.setLevel(logging.DEBUG)
+    werkzeug_logger.setLevel(logging.DEBUG)
+    application_logger.info("Running in development mode. Logging with level DEBUG")
 elif os.environ.get('ENVIRONMENT') == 'test':
-    logger.setLevel(logging.INFO)
-    logger.info("Running in test mode. Logging with level INFO")
+    application_logger.setLevel(logging.INFO)
+    werkzeug_logger.setLevel(logging.INFO)
+    application_logger.info("Running in test mode. Logging with level INFO")
 elif os.environ.get('ENVIRONMENT') == 'prod':
-    logger.setLevel(logging.INFO)
-    logger.info("Running in production mode. Logging with level INFO")
+    application_logger.setLevel(logging.INFO)
+    werkzeug_logger.setLevel(logging.INFO)
+    application_logger.info("Running in production mode. Logging with level INFO")
