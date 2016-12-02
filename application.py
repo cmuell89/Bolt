112d640df483ef2022bcfb1fb1f4838821dfd76f
Carl Mueller <cmuell89@gmail.com>
2016-12-02 12:13:28 -0800

    Papertrails logging.

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ import os
 from app import application
 
 application.secret_key = os.environ.get('SECRET_KEY')
+
 # run the app according to the ENVIRONMENT env variable.
 if __name__ == "__main__": 
     if os.environ.get('ENVIRONMENT') == 'dev':
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 from os.path import join, dirname
 from dotenv import load_dotenv
-import logging
+import logging.handlers
 import sys
 import os
 
@@ -11,20 +11,28 @@ ENVIRONMENT
 if os.path.isfile(os.path.join(os.path.dirname(__file__), '.env')):
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
+
 """
 LOGGING
 """
+werkzeug_logger = logging.getLogger('werkzeug')
 logger = logging.getLogger('BOLT')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 stdoutHandler = logging.StreamHandler(sys.stdout)
 stdoutHandler.setFormatter(formatter)
-
-fileHandler = logging.FileHandler("{0}/{1}.log".format(os.path.join(os.path.dirname(__file__), './logs'), 'stdout'))
-fileHandler.setFormatter(formatter)
-
-logger.addHandler(fileHandler)
 logger.addHandler(stdoutHandler)
+
+if os.environ.get('ENVIRONMENT') == 'dev':
+    fileHandler = logging.FileHandler("{0}/{1}.log".format(os.path.join(os.path.dirname(__file__), './logs'), 'stdout'))
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler)
+
+paperTrailsHandler = logging.handlers.SysLogHandler(address=(os.environ.get('PAPERTRAILS_ADDRESS')
+                                                             , int(os.environ.get('PAPERTRAILS_PORT'))))
+paperTrailsHandler.setFormatter(formatter)
+logger.addHandler(paperTrailsHandler)
+werkzeug_logger.addHandler(paperTrailsHandler)
 
 if os.environ.get('ENVIRONMENT') == 'dev':
     logger.setLevel(logging.DEBUG)
