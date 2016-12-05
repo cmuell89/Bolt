a3fcc1ac530b601908a23ce3654065b1113c784e
Carl Mueller <cmuell89@gmail.com>
2016-12-05 15:34:44 -0800

    Regexers now return string value, not an array of matches

--- nlp/ner/regexer.py
+++ nlp/ner/regexer.py
@@ -6,11 +6,10 @@ class Regexer:
         self.regex_list = regex_list
 
     def get_matches(self, query):
-        results = []
         for pattern in self.regex_list:
             regex = re.compile(pattern)
             result = regex.search(query)
             if result is not None:
                 match = result.group(0)
-                results.append(match)
-        return results
+                return match
+        return None
