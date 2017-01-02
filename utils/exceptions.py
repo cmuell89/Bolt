0b08ebb364bdaac6596b05e809448e36ff5bcb13
cmuell89 <cmuell89@gmail.com>
2017-01-02 15:05:37 -0800

    new exceptions

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -27,3 +27,42 @@ class AnnotatorValidationError(Exception):
 
     def __str__(self):
         return repr(self.value)
+
+
+class AnnotatorAnnotateError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class ClassificationModelError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class GazetteerModelError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class AnalyzerError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+class UpdaterError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
