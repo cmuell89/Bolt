6ec6c522d894c89a5b91daa77e620f05857fcc42
Carl <cmuell89@gmail.com>
2017-02-14 21:55:03 -0800

    basic boot shell script executed upon server startup

--- config/docker/production/boot.sh
+++ config/docker/production/boot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-dir="/home/app/bolt"
-logfile="/var/log/bolt.log"
+dir="/home/app/Bolt"
+logfile="/var/log/Bolt.log"
 
 touch "$logfile"
 chown -R app:app "$logfile"
@@ -9,12 +9,4 @@ chown -R app:app "$logfile"
 mkdir -p /var/log/nginx/
 chown -R www-data:adm /var/log/nginx/
 
-#su - app <<EOF
-#cd /home/app/bolt
-#source env/bin/activate
-#pip install -r requirements.txt
-#deactivate
-#EOF
-
 chown -R app:app "$dir"
-source /etc/environment
